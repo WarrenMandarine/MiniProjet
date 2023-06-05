d32a772c95326f8b9872c8f66ab00bf4ebf5556e
WarrenMandrine <warren.jeuffrain@gmail.com>
2023-06-05 16:01:00 +0200

    nouveautés

--- Mini Projet Cours Modale/script.js
+++ Mini Projet Cours Modale/script.js
@@ -1,25 +1,38 @@
-let AfficheLivre = document.querySelector("livre");
+let afficheLivre = document.querySelector(".Livre");
 
-AfficheLivre.addEventListener("click", () =>{
+afficheLivre.addEventListener("click", ()=>{
+ 
 
+    //création
     const voile = document.createElement("div");
     const modale = document.createElement("div");
     const croix = document.createElement("div");
     const imgModale = document.createElement("img");
 
+    //ajout id
     voile.id = "voile";
     modale.id ="modale";
     croix.id = "croix";
     imgModale.id = "imgModale";
 
-
+// inject element
     document.body.appendChild(voile);
     document.body.appendChild(modale);
 
-    voile.textContent = "Apparition du voile";
+    //ajout données dans l'élément
     croix.textContent = "X"
     imgModale.setAttribute("src", "./src/Illustration(1).png");
     modale.append(croix, imgModale);
+
+    let datas = afficheLivre.dataset;
+    for(key in datas){
+        console.log(datas[key]);
+        const parag = document.createElement("p");
+        parag.textContent = datas[key];
+        
+        modale.appendChild(parag);
+    }
+
 
     croix.addEventListener("click", ()=>{
 
@@ -27,3 +40,5 @@ AfficheLivre.addEventListener("click", () =>{
         modale.remove();
     })
 });
+
+
